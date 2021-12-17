8809d7768b5314a5734333e635734bd2e50acc02
Cosdaman <Cosdaguy@gmail.com>
2021-12-16 20:43:54 -0800

    added handlebars placeholders and setup for cookies

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
-
+const session = require('express-session');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
@@ -10,13 +10,24 @@ const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({});
 
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
