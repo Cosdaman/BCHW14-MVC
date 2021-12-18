0711f1866ba3e4ee85574bc37c4783f07543a3f7
Cosdaman <Cosdaguy@gmail.com>
2021-12-17 16:19:12 -0800

    fixed dashboard

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const apiroute = require('./api')
 const routes = require('./routes.js');
+const dashboard = require('./dashboardRoutes')
 
 router.use('/api', apiroute);
 router.use('/', routes);
+router.use('/dashboard', dashboard)
 
 module.exports = router;
