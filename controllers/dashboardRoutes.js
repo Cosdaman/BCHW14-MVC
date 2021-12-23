bbcbde799d05a490f4a8b868f7f4898e99a3dad5
Cosdaman <Cosdaguy@gmail.com>
2021-12-23 11:27:59 -0800

    starting dashboard display

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -5,7 +5,16 @@ const withAuth = require('../utils/auth');
 
 router.get('/', withAuth, async (req, res) => {
     try {
-        res.render('dashboard', { logged_in: req.session.logged_in });
+        const dbBlogData = await Blogpost.findAll(
+            {
+                raw: true,
+                where: {
+                    user_id: req.session.user_id
+                }
+            }
+        );
+        console.log(dbBlogData)
+        res.render('dashboard', { dbBlogData, logged_in: req.session.logged_in });
     } catch (err) {
         console.log(err);
         res.status(500).json(err);
